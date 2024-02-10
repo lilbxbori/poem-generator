36df0cd6930d14de59d0de93710fc884ec00af18
lilbxbori <156120860+lilbxbori@users.noreply.github.com>
2024-02-09 20:44:30 -0500

    Added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector("#poem");
+
+  new Typewriter("#poem", {
+    strings: "La bomba ay que rica es!",
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
