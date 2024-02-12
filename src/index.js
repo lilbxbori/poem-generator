309818e40c488ce1a231c745c6ed87aa275fbf41
lilbxbori <156120860+lilbxbori@users.noreply.github.com>
2024-02-12 12:16:27 -0500

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -12,14 +12,18 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  // let poemElement = document.querySelector("#poem");
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "01c14dcf0dc6te3ab9c3fo9f900f34e9";
 
   let context =
-    "You are a devouted christian and love God with all your life. Your mission to to generate 4 lines poem in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions. Do not include a title. Sign the poem with <strong>SheCodes AI</strong> always at the end of the poem.";
+    "You are a devoted Christian and love God with all your life. Your mission to generate a 4 lines poem in basic HTML format and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title. Sign the poem with <strong>SheCodes AI</strong> always at the end of the poem.";
   let prompt = `User instructions: Generate a catholic poem dedicated to God about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink"> ⏳  Generating Christian Poem about ${instructionsInput.value} 😇</div>`;
+
   console.log("Generating Poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
