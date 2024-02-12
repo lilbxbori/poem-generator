943be1e35b3bd696383cf7666cd1adfd51938f96
lilbxbori <156120860+lilbxbori@users.noreply.github.com>
2024-02-12 11:11:56 -0500

    Added AI API integration and axios

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
+function displayPoem(response) {
+  console.log("poem generated");
 
   new Typewriter("#poem", {
-    strings: "La bomba ay que rica es!",
+    strings: response.data.answer,
     autoStart: true,
     delay: 40,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  // let poemElement = document.querySelector("#poem");
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "01c14dcf0dc6te3ab9c3fo9f900f34e9";
+
+  let context =
+    "You are a devouted christian and love God with all your life. Your mission to to generate 4 lines poem in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions. Do not include a title. Sign the poem with <strong>SheCodes AI</strong> always at the end of the poem.";
+  let prompt = `User instructions: Generate a catholic poem dedicated to God about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("Generating Poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
